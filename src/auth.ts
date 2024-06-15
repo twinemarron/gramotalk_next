7f4bd40da4129f87a8b2505ded9b9f703821bb19
twinemarron <twinemarron@gmail.com>
2024-06-15 23:48:29 +0900

    Add basePath

--- src/auth.ts
+++ src/auth.ts
@@ -11,13 +11,8 @@ import {
 } from "@/server/db/schema";
 
 export const { signIn, signOut, auth, handlers } = NextAuth({
+  basePath: "/api/auth",
   providers: [Google],
-  // pages: {
-  //   signIn: "/login",
-  // },
-  pages: {
-    signIn: "/signin",
-  },
   // users table に追加したカラムを使用するため、第二引数に独自スキーマを渡す
   adapter: DrizzleAdapter(db, {
     usersTable: users,
